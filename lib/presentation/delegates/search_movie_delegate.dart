5bef78bbbe05f3bf64fd8ea35462f1599af90a11
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-09-17 10:59:04 -0700

    update: search delegate

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,17 +1,27 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+import '../../domain/entities/movie.dart';
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return const [Text('buildActions')];
+    return [
+      FadeIn(
+          animate: query.isNotEmpty,
+          child: IconButton(
+              onPressed: () => query = '', icon: const Icon(Icons.clear)))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back));
   }
 
   @override
