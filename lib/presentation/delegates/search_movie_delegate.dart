ef3957a84664cb4493021d816f826a088f161097
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-09-17 11:48:59 -0700

    bug-fix: fecha de estreno null

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 
 import '../../domain/entities/movie.dart';
 
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback movieCallback;
+
+  SearchMovieDelegate({required this.movieCallback});
+
   @override
   String get searchFieldLabel => 'Buscar';
 
@@ -31,6 +37,20 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('buildSuggestions');
+    return FutureBuilder(
+        future: movieCallback(query),
+        initialData: const [],
+        builder: (context, snapshot) {
+          final movies = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return ListTile(
+                title: Text(movie.title),
+              );
+            },
+          );
+        });
   }
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,12 +1,14 @@
+import 'package:cinemapedia/presentation/providers/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../delegates/search_movie_delegate.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
@@ -29,8 +31,11 @@ class CustomAppbar extends StatelessWidget {
               const Spacer(),
               IconButton(
                   onPressed: () {
+                    final movieReposiotry = ref.read(movieRepositoryProvider);
                     showSearch(
-                        context: context, delegate: SearchMovieDelegate());
+                        context: context,
+                        delegate: SearchMovieDelegate(
+                            movieCallback: movieReposiotry.searchMovies));
                   },
                   icon: const Icon(Icons.search))
             ],
