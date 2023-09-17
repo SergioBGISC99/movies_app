070b21948f3887ba85a86664f005dc2e6bc99adf
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-09-17 12:32:20 -0700

    update: serach delegate style

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,7 +16,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: moviedb.posterPath != ''
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://www.content.numetro.co.za/ui_images/no_poster.png',
       releaseDate:
           moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
       title: moviedb.title,
--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
+import '../../config/helpers/human_formats.dart';
 import '../../domain/entities/movie.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
@@ -43,14 +44,80 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         builder: (context, snapshot) {
           final movies = snapshot.data ?? [];
           return ListView.builder(
-            itemCount: movies.length,
-            itemBuilder: (context, index) {
-              final movie = movies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
-          );
+              itemCount: movies.length,
+              itemBuilder: (context, index) =>
+                  _MovieSearchItem(movie: movies[index]));
         });
+  }
+}
+
+class _MovieSearchItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieSearchItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * .2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                loadingBuilder: (context, child, loadingProgress) =>
+                    FadeInDown(child: child),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: size.width * .6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  style: textStyles.titleMedium,
+                  textAlign: TextAlign.start,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  movie.overview,
+                  style: textStyles.bodySmall,
+                  textAlign: TextAlign.justify,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded,
+                        color: Colors.yellow.shade800),
+                    const SizedBox(width: 3),
+                    Text(HumanFormats.number(movie.voteAverage),
+                        style: textStyles.bodyMedium
+                            ?.copyWith(color: Colors.yellow.shade800)),
+                    const Spacer(),
+                    Text(
+                      HumanFormats.number(movie.popularity),
+                      style: textStyles.bodySmall,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
