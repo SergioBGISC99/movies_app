aa1ec52001f77d68abf1e3b2b647227058407720
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-09-23 13:52:06 -0700

    update: shell route de go router

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -3,15 +3,14 @@ import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
-  const HomeScreen({super.key});
+  final Widget childView;
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
-      ),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
