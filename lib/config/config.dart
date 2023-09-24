08ce99565091f1bf576f5998c38679f5c7a449ec
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-09-24 12:36:47 -0700

    change: formato de fecha

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,9 +1,6 @@
 export 'constants/environment.dart';
 export 'constants/formatters.dart';
 
-export 'enums/months.dart';
-export 'enums/weekdays.dart';
-
 export 'helpers/human_formats.dart';
 
 export 'router/app_router.dart';
